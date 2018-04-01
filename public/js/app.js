095ca81d3b5abf193c41db505ceaa8432788875b
LilliamHaro <qualia950@gmail.com>
2018-04-01 14:47:54 -0500

    subiendo avance

--- public/js/app.js
+++ public/js/app.js
@@ -15,6 +15,80 @@ function addMarkersToMap(map) {
 }
 }
 
+
+
+
+
+$('#btn').on('click', function(event) {
+alert('holaa')
+//lamando a la funcion que mostrara la ruta en el mapa
+calculateRouteFromAtoB(platform);
+
+})
+
+function calculateRouteFromAtoB (platform) {
+  var router = platform.getRoutingService(),
+    routeRequestParams = {
+      mode: 'fastest;car',
+      representation: 'display',
+      routeattributes : 'waypoints,summary,shape,legs', 
+      maneuverattributes: 'direction,action',
+      waypoint0: '52.5160,13.3779', // punto 1
+      waypoint1: '52.5206,13.3862'  // punto 2
+    };
+
+
+  router.calculateRoute(
+    routeRequestParams,
+    onSuccess,
+    onError
+  );
+}
+
+
+function onSuccess(result) {
+  var route = result.response.route[0];
+  addRouteShapeToMap(route);
+  
+}
+
+function onError(error) {
+  alert('Ooops!');
+}
+
+
+function addRouteShapeToMap(route){
+  var lineString = new H.geo.LineString(),
+    routeShape = route.shape,
+    polyline;
+
+  routeShape.forEach(function(point) {
+    var parts = point.split(',');
+    lineString.pushLatLngAlt(parts[0], parts[1]);
+  });
+
+  polyline = new H.map.Polyline(lineString, {
+    style: {
+      lineWidth: 4,
+      strokeColor: 'rgba(0, 128, 255, 0.7)'
+    }
+  });
+  // añadiendo la linea al mapa
+  map.addObject(polyline);
+  // zoom al lugar
+  map.setViewBounds(polyline.getBounds(), true);
+}
+
+
+
+
+
+
+
+
+
+
+
 //Inicializando el mapa
 var platform = new H.service.Platform({
   app_id: 'DemoAppId01082013GAL',
